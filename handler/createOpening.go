43640f979d07b3cb25165e758f8c01fd34f15bea
BTIwamoto <bruno.ivamoto@gmail.com>
2023-08-19 16:17:57 -0300

    :sparkles: Add create opening handler logic

--- handler/createOpening.go
+++ handler/createOpening.go
@@ -15,6 +15,7 @@ func CreateOpeningHandler(ctx *gin.Context) {
 	if err := request.Validate(); err != nil {
 		logger.Errorf("validation error: %v", err.Error())
 		sendError(ctx, http.StatusBadRequest, err.Error())
+		return
 	}
 
 	opening := schemas.Opening{
@@ -29,6 +30,7 @@ func CreateOpeningHandler(ctx *gin.Context) {
 	if err := db.Create(&opening).Error; err != nil {
 		logger.Errorf("error creating opening: %v", err.Error())
 		sendError(ctx, http.StatusInternalServerError, err.Error())
+		return
 	}
 
 	sendSuccess(ctx, "create-opening", opening)
