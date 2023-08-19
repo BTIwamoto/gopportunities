9f6dd7aafff7644380fad79959788cb89208a3e9
BTIwamoto <bruno.ivamoto@gmail.com>
2023-08-19 16:17:28 -0300

    :sparkles: Add update opening handler logic

--- handler/request.go
+++ handler/request.go
@@ -40,3 +40,23 @@ func (c *CreateOpeningRequest) Validate() error {
 	}
 	return nil
 }
+
+// UpdateOpening
+type UpdateOpeningRequest struct {
+	Role     string `json:"role"`
+	Company  string `json:"company"`
+	Location string `json:"location"`
+	Remote   *bool  `json:"remote"`
+	Link     string `json:"link"`
+	Salary   int64  `json:"salary"`
+}
+
+func (u *UpdateOpeningRequest) Validate() error {
+	// if any field is provided, validation is truthy
+	if u.Role != "" || u.Company != "" || u.Location != "" || u.Remote != nil || u.Link != "" || u.Salary > 0 {
+		return nil
+	}
+
+	// if none oof the fields werer provided, return falsy
+	return fmt.Errorf("at least one valid field must be provided")
+}
