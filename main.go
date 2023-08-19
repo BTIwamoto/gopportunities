ca09203c315a223efa9e537ada40727d18a71716
BTIwamoto <bruno.ivamoto@gmail.com>
2023-08-19 12:59:38 -0300

    :sparkles: Add custom logger :construction:

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "github.com/BTIwamoto/gopportunities/router"
+import (
+	"github.com/BTIwamoto/gopportunities/config"
+	"github.com/BTIwamoto/gopportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+
+	// Initialize Configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
+
+	// Initialize Router
 	router.Initialize()
 }
