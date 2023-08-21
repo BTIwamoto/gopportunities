299d1ad39cc97e760fb6bf6238db30c49b8cd23b
BTIwamoto <bruno.ivamoto@gmail.com>
2023-08-21 07:00:27 -0300

    :memo: Add swagger delete opening configuration

--- handler/deleteOpening.go
+++ handler/deleteOpening.go
@@ -8,6 +8,18 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// @BasePath /api/v1
+
+// @Summary Delete opening
+// @Description Delete a new job opening
+// @Tags Openings
+// @Accept json
+// @Produce json
+// @Param request body DeleteOpeningRequest true "Request body"
+// @Success 200 {object} DeleteOpeningResponse
+// @Failure 400 {object} ErrorResponse
+// @Failure 404 {object} ErrorResponse
+// @Router /opening [POST]
 func DeleteOpeningHandler(ctx *gin.Context) {
 	id := ctx.Query("id")
 	if id == "" {
